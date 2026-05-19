344d67e07f7bc50416b249b58f265254a8597208
宋江龙 <jianglong.song@glory.com>
2026-05-19 15:27:31 +0800

    feat: 新增打包应用成apk安装包功能

---
+++ android/app/src/main/java/com/sjl/space/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sjl.space;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sjl.space',
+  appName: 'sjl-space',
+  webDir: 'dist'
+};
+
+export default config;
